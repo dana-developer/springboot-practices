12ccf941d51639a48588fdd6a3a1667984a6c654
dana-developer <hdy78901@naver.com>
2025-01-21 10:14:18 +0900

    springboot-example 예시 추가 및 springboot-myapplication init #1

--- sprintboot-example/src/main/java/ex02/ExApplication.java
+++ sprintboot-example/src/main/java/ex02/ExApplication.java
@@ -1,11 +1,14 @@
-package ex01;
+package ex02;
 
 import java.util.Map;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+
+import ex02.runner.HelloWorldRunner;
 
 @SpringBootApplication
 public class ExApplication {	// Bootstrapping Class
@@ -15,10 +18,14 @@ public class ExApplication {	// Bootstrapping Class
 		return Map.of("greetings", "Hello World");
 	}
 	
+	@Bean
+	public ApplicationRunner applicationRunner() {
+		return new HelloWorldRunner(myMap());
+	}
+	
 	public static void main(String[] args) {
 	
 		try (ConfigurableApplicationContext ac = SpringApplication.run(ExApplication.class, args)){
-			System.out.println("ExApplication runs ........ " + ac.getBean("myMap", Map.class).get("greetings"));
 		}
 		
 	}
